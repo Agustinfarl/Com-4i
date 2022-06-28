aef808afbe695d023b340049f26fda69035ad525
Agustin Rodriguez <oyifarl@gmail.com>
2022-06-27 21:56:32 -0300

    clase 27-06-2022

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
 const constante= true
 
 // var: es reasignable y redeclarable. NO ES RECOMENDABLE
-var numero = 0
-var numero = 5
-var numero = true
+var numero5 = 0
+var numero5 = 5
+var numero5 = true
 
 // let: puede ser reasignable pero no es REDECLARABLE
 
@@ -22,3 +22,35 @@ const IVA = [21, 10.5, 27]
 // con ".push" puedo forzar entrar un dato nuevo a la constante
 
 IVA.push(15)
+
+
+
+// evaluar si un numero es par o impar
+// la funcion PARSEINT es para pasar una variable a NUMERO ENTERO, saca lo que esta despues del .
+
+let numero = 4.7
+
+let resto = parseInt(numero % 2)
+
+if(resto === 0){
+    console.log("el numero es par")
+}
+else{
+    console.log("el numero es impar")
+}
+
+//dada una edad, evaluar si la persona es mayor
+// si la persona no tiene 18 años o mas
+// si la persona no tiene 21 años o mas no puede comprar alcohol
+
+let edad= prompt("ingresa tu edad")
+
+if ( edad < 18) {
+  console.log('No puede ingresar')
+}
+else if (edad < 21 ) {
+  console.log('Puede ingresar pero no puede comprar bebidas alcoholicas')
+}
+else {
+  console.log('Puede ingresar y puede comprar bebidas alcoholicas')
+}
