e2defe71ceca08675d4d9079df6ee20e828744e4
Agustin Rodriguez <oyifarl@gmail.com>
2022-06-27 21:25:16 -0300

    hola

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// declarar una constante, NO SE PUEDE REASIGNAR NI REDECLARAR
+
+const constante= true
+
+// var: es reasignable y redeclarable. NO ES RECOMENDABLE
+var numero = 0
+var numero = 5
+var numero = true
+
+// let: puede ser reasignable pero no es REDECLARABLE
+
+let string = "texto"
+let number = 5
+let boolean = true
+
+
+// array "arreglo" es un tipo de variable que almacena varios datos, pero sin identificar variables
+
+const IVA = [21, 10.5, 27]
+
+// con ".push" puedo forzar entrar un dato nuevo a la constante
+
+IVA.push(15)
